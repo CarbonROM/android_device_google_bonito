31e826ba96f484e19c319e9062a6ea95d2132e20
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2020-06-02 07:32:55 +0000

    Update build fingerprint to QQ3A.200605.002
    Change-Id: Ib436042692a5c90596771f810911c325a778a3ac
    Signed-off-by: Joe Maples <joe@maples.dev>

--- carbon_bonito.mk
+++ carbon_bonito.mk
@@ -22,8 +22,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bonito \
-    PRIVATE_BUILD_DESC="bonito-user 10 QQ1A.200205.002 6084386 release-keys"
+    PRIVATE_BUILD_DESC="bonito-user 10 QQ3A.200605.002 6416773 release-keys"
 
-BUILD_FINGERPRINT := google/bonito/bonito:10/QQ1A.200205.002/6084386:user/release-keys
+BUILD_FINGERPRINT := "google/bonito/bonito:QQ3A.200605.002/6416773:user/release-keys"
 
 $(call inherit-product-if-exists, vendor/google/bonito/bonito-vendor.mk)
